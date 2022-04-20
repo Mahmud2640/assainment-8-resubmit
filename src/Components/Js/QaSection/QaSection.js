44cc3f60268d4f7c91f6a9bc4e019a993c8cbe81
Mahmud2640 <mdmahmud484@gmail.com>
2022-04-21 00:24:07 +0600

    project setup

---
+++ src/Components/Js/QaSection/QaSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QaSection = () => {
+   return (
+      <div>
+         <h1>This Is QA section</h1>
+      </div>
+   );
+};
+
+export default QaSection;
---
+++ src/Components/Js/SideBar/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBar = () => {
+   return (
+      <div>
+         <h1>Hello this is sidebar.</h1>
+      </div>
+   );
+};
+
+export default SideBar;
